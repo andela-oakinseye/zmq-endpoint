3352d053c6e28fb7c4eb41d7e5908635084340df
andela-oakinseye <olawale.akinseye@andela.com>
2018-04-11 21:56:42 +0100

    document shit

--- zmq/publish.js
+++ zmq/publish.js
@@ -1,8 +1,7 @@
 import zeromq from 'zeromq';
 
-const pubber = zeromq.socket('pub');
-
 const publish = (port, message) => {
+  const pubber = zeromq.socket('pub');
   const endpoint = `tcp://35.176.255.14:${port}`;
   pubber.connect(endpoint);  
   pubber.send(message);
