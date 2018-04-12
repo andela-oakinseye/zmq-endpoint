ed1954db6efef944ae870ca3113550a9eb2b5c4b
salvatore-fxpig <salvatore@fxpig.com>
2018-04-12 15:55:57 +0200

    ...

--- zmq/publish.js
+++ zmq/publish.js
@@ -4,13 +4,20 @@ const publish = (port, message) => {
   const pubber = zeromq.socket('pub');
   const endpoint = `tcp://35.176.255.14:${port}`;
   pubber.connect(endpoint);  
+  pubber.monitor();
   pubber.on('connect', () => {
     console.log('Connection established');
+    console.log('Message Published');
     pubber.send(message);
     pubber.close();
   });
+  pubber.on('connect_delay', () => {
+    console.log('connect_delay');
+  });
+  pubber.on('connect_retry', () => {
+    console.log('connect_retry');
+  });
 
-  console.log('Message Published');
 };
 
 export default publish;
