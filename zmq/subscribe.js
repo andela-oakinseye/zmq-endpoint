34598a868a2d87d70b4219639f28d985afe59e69
andela-oakinseye <olawale.akinseye@andela.com>
2018-04-12 15:52:49 +0100

    fix issues with pub and sub

--- zmq/subscribe.js
+++ zmq/subscribe.js
@@ -4,11 +4,14 @@ const subscribe = (port, cb) => {
   const subber = zeromq.socket('sub');
   const endpoint = `tcp://35.176.255.14:${port}`
   subber.connect(endpoint);
-  subber.subscribe('');
+  subber.monitor();
+  subber.on('connect', () => {
+    console.log(`Connection to ${port} established`)
+     subber.subscribe('');
+  });
+ 
   subber.on('message', (message) => {
-    // console.log(message);
-    // console.log(`New message from ${port}`);
-    return cb(message);
+    cb(message);
   });
 };
 
