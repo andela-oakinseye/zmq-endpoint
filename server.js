29bc9619b8e06ae3ba4558e88d23eb32ac107886
Fortune Ekeruo <legalmody@gmail.com>
2018-07-11 18:20:15 +0100

    Integrate protobufjs serializer and update zmq ports

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import capnp from 'capnp';
+import protobuf from 'protobufjs';
 
 import fs from 'fs';
 import path from 'path';
 
-import schema from './zmq/schema.capnp';
+// import schema from './zmq/schema.capnp';
 import { publish, subscribe } from './zmq';
 
 const app = express();
@@ -18,31 +18,61 @@ const subPorts = {
     barProvSubber: 5561
 }
 
+// Protobuf parser
+const ENCODE = 'ENCODE';
+const DECODE = 'DECODE';
+let APIMessage;
+
+protobuf.load("schema.proto")
+  .then((root) => {
+    APIMessage = root.lookupType('scheme.APIMessage');
+  });
+
+const protobufParser = (pattern, message) => new Promise((resolve, reject) => {
+  if (pattern === DECODE) {
+    const decoded = APIMessage.decode(message);
+    return resolve(decoded);
+  }
+
+  if (pattern === ENCODE) {
+    const buffer = APIMessage.encode(APIMessage.create(message)).finish();
+    return resolve(buffer);
+  }
+
+  reject(Error('Unable to endode or decode'));
+});
 
 function writeOutput(messagePORT, message) {
-  const newMessage = JSON.stringify(capnp.parse(schema.APIMessage, message));
-  console.log(newMessage);
-    return fs.appendFile(`outputs/port-${messagePORT}.txt`,  `${newMessage} \n`, (err) => {
-        if (err) throw err;
-      })
+//   const newMessage = JSON.stringify(capnp.parse(schema.APIMessage, message));
+//   console.log(newMessage);
+//     return fs.appendFile(`outputs/port-${messagePORT}.txt`,  `${newMessage} \n`, (err) => {
+//         if (err) throw err;
+//       })
+
+    protobufParser(DECODE, message).then((res) => {
+        // return fs.appendFile(`outputs/port-${messagePORT}.txt`,  `${res} \n`, (err) => {
+        //             if (err) throw err;
+        //         });
+        console.log('>>>>>>>>>>>>', res);
+    }).catch(err => console.log(err));
 }
 
 
 Object.values(subPorts).forEach((port) => {
     subscribe(port, (message) => {
         switch(port) {
-            case 5500:
-                return writeOutput(5500, message);
+            case 5559:
+                return writeOutput(5559, message);
+                break;
+            case 5552:
+                return writeOutput(5552, message);
                 break;
             case 5553:
                 return writeOutput(5553, message);
                 break;
-            case 5559:
-                return writeOutput(5559, message);
-                break;
-            case 5561:
-               return writeOutput(5561, message);
-                break;
+            case 5557:
+               return writeOutput(5557, message);
+               break;
         }
     })
 })
@@ -56,9 +86,13 @@ app.post('/', (req, res) => {
     const port = Number(req.body.port);
     const message = JSON.parse(req.body.message);
     console.log(JSON.stringify(message, null, 1));
-    const serialedData = capnp.serialize(schema.APIMessage, message)
-    publish(port, serialedData);
-    return res.send('Message Published');
+    // const serialedData = capnp.serialize(schema.APIMessage, message)
+    // publish(port, serialedData);
+    // return res.send('Message Published');
+    protobufParser(ENCODE, message).then((response) => {
+        publish(port, response);
+        return res.send('Message Published');
+    }).catch((err) => console.log(err));
 })
 
 app.listen(3200, () => {
