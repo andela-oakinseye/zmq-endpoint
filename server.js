4abc1da718f788abe0df23692089241c5a5d7bbb
Fortune Ekeruo <legalmody@gmail.com>
2018-07-20 13:34:04 +0100

    Refactor publish to dealer and write dealer outputs

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import protobuf from 'protobufjs';
+import zeromq from 'zeromq';
+import { log } from 'util';
+
 
 import fs from 'fs';
 import path from 'path';
@@ -10,6 +13,10 @@ import { publish, subscribe } from './zmq';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const ZMQordersDEAL = zeromq.socket('dealer'),
+  ZMQdbDEAL = zeromq.socket('dealer'),
+  ZMQbarDEAL = zeromq.socket('dealer');
 
 // const subPorts = {
 //   newOrderSubber: 5500,
@@ -50,12 +57,16 @@ const protobufParser = (pattern, message) => new Promise((resolve, reject) => {
 
 function writeOutput(messagePORT, message) {
   protobufParser(DECODE, message).then((res) => {
+    if (res.bookresponse) {
+      console.log('>>>>>>>>>>')
+      return null;
+    }
     return fs.appendFile(`outputs/port-${messagePORT}.txt`, `${JSON.stringify(res)} \n \n`, (err) => {
-      
-      console.log(">>>>>>>>>>>>", messagePORT);
-      if (err) throw err;
-    });
-  }).catch(err => console.log(err));
+  
+        console.log(">>>>>>>>>>>>", messagePORT);
+        if (err) throw err;
+      });
+    }).catch(err => console.log(err));
 }
 
 // ZMQ subscription object instances for each sub ports
@@ -74,6 +85,46 @@ Object.values(subPorts).forEach((port) => {
   });
 })
 
+const dealerPorts = {
+  orderDealer: 5553,
+  dbDealer: 5551,
+  barDealer: 5556
+};
+
+const ZMQdealerInit = () => Promise.all([
+  publish(ZMQordersDEAL, dealerPorts.orderDealer, (...ZMQmessage) => {
+    log('Something on ZMQordersDEAL');
+    ZMQmessage = ZMQmessage[1];
+    writeOutput(dealerPorts.orderDealer, ZMQmessage);
+    protobufParser(DECODE, ZMQmessage).then((message) => {
+      log('Message parsed by protobuf:: ', message);
+      // Nothing worthwhile to listen to on this channel
+    }).catch(err => log(err));
+  }),
+  publish(ZMQdbDEAL, dealerPorts.dbDealer, (...ZMQmessage) => {
+    log('Something on ZMQdbDEAL');
+    ZMQmessage = ZMQmessage[1];
+    writeOutput(dealerPorts.dbDealer, ZMQmessage);
+    protobufParser(DECODE, ZMQmessage).then((message) => {
+      log('Message parsed by protobuf:: ', message);
+      if (message.syncResponse) {
+        receiveSync(message.syncResponse);
+      } else if (message.activeOrdersResponse) {
+        message.activeOrdersResponse.activeOrders.orders.forEach(order => receiveOrderStatus(order, true));
+      }
+    }).catch(err => log(err));
+  }),
+  publish(ZMQbarDEAL, dealerPorts.barDealer, (...ZMQmessage) => {
+    log('Something on ZMQbarDEAL');
+    ZMQmessage = ZMQmessage[1];
+    writeOutput(dealerPorts.barDealer, ZMQmessage);
+    protobufParser(DECODE, ZMQmessage).then((message) => {
+      log('Message parsed by protobuf:: ', message);
+      // later
+    }).catch(err => log(err));
+  })
+]);
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname) + '/index.html');
@@ -87,11 +138,46 @@ app.post('/', (req, res) => {
   // const serialedData = capnp.serialize(schema.APIMessage, message)
   // publish(port, serialedData);
   // return res.send('Message Published');
-  protobufParser(ENCODE, message).then((response) => {
-    publish(port, response);
-    return res.send('Message Published');
-  }).catch((err) => console.log(err));
+  // protobufParser(ENCODE, message).then((response) => {
+  //   publish(port, response);
+  //   return res.send('Message Published');
+  // }).catch((err) => console.log(err));
+
+
+
+  // publish(ZMQordersDEAL, port, (...ZMQmessage) => {
+  //   log('Something on ZMQordersDEAL');
+  //   ZMQmessage = ZMQmessage[1];
+  //   protobufParser(DECODE, ZMQmessage).then((message) => {
+  //     log('Message parsed by protobuf:: ', message);
+  //     // Nothing worthwhile to listen to on this channel
+  //   }).catch(err => log(err));
+  // })
+
+  let ZMQsocket;
+  if (message.newOrderRequest || message.orderCancelRequest || message.orderStatusRequest) {
+    ZMQsocket = ZMQordersDEAL;
+  } else if (message.activeOrdersRequest || message.syncRequest || message.exchangesRequest) {
+    ///Avoids huge cascades of syncRequest messages
+    if (message.syncRequest && ZMQdbDEAL.busySyncing)
+      return;
+    else {
+      setTimeout(() => ZMQdbDEAL.busySyncing = false, 1000);
+      ZMQdbDEAL.busySyncing = true;
+    }
+    ZMQsocket = ZMQdbDEAL;
+  } else if (message.barRequest) {
+    ZMQsocket = ZMQbarDEAL;
+  }
+
+  log(`Sending to core: ${JSON.stringify(message)}`);
+  protobufParser(ENCODE, message).then((buffer) => {
+    ZMQsocket.send(["", buffer]);
+    res.send('Message Published');
+  }).catch(err => log(err));
 })
+
+ZMQdealerInit();
 
 app.listen(3200, () => {
   console.log('Running on port 3200')
--- zmq/publish.js
+++ zmq/publish.js
@@ -1,23 +1,39 @@
+import { log } from 'util';
 import zeromq from 'zeromq';
 
-const publish = (port, message) => {
-  const ZMQdealer = zeromq.socket('dealer');
-  const endpoint = `tcp://35.176.255.14:${port}`;
-  ZMQdealer.connect(endpoint);
-  ZMQdealer.monitor();
-  ZMQdealer.on('connect', () => {
-    console.log('Connection established');
-    console.log('Message Published');
-    ZMQdealer.send(message);
-    ZMQdealer.close();
-  });
-  ZMQdealer.on('connect_delay', () => {
-    console.log('connect_delay');
-  });
-  ZMQdealer.on('connect_retry', () => {
-    console.log('connect_retry');
-  });
+// const publish = (port, message) => {
+//   const ZMQdealer = zeromq.socket('dealer');
+//   const endpoint = `tcp://35.176.255.14:${port}`;
+//   console.log('Yo Im the ZMQ Dealer::::::::', ZMQdealer);
+  
+//   ZMQdealer.connect(endpoint);
+//   ZMQdealer.monitor();
+//   ZMQdealer.on('connect', () => {
+//     console.log('Connection established');
+//     console.log('Message Published');
+//     ZMQdealer.send(["", message]);
+//     ZMQdealer.close();
+//   });
+//   ZMQdealer.on('connect_delay', () => {
+//     console.log('connect_delay');
+//   });
+//   ZMQdealer.on('connect_retry', () => {
+//     console.log('connect_retry');
+//   });
 
-};
+// };
+
+// export default publish;
+
+const publish = (dealer, port, cb) => new Promise((resolve) => {
+  const endpoint = `tcp://35.176.255.14:${port}`;
+  dealer.connect(endpoint);
+  dealer.monitor();
+  dealer.on('connect', () => {
+    log(`Connection established on port ${port}`);
+    resolve();
+  });
+  dealer.on('message', cb);
+});
 
 export default publish;
