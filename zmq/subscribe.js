4b3ab030cd6bf687bc2cd60d0e7ad25438f4047b
Mel Michael <mel.michael@andela.com>
2018-07-20 19:30:03 +0100

    code clean up

--- zmq/subscribe.js
+++ zmq/subscribe.js
@@ -1,19 +1,4 @@
-// import zeromq from 'zeromq';
 const { log } = console;
-// const subscribe = (port, cb) => {
-//   const ZMQsub = zeromq.socket('sub');
-//   const endpoint = `tcp://35.176.255.14:${port}`
-//   ZMQsub.connect(endpoint);
-//   ZMQsub.monitor();
-//   ZMQsub.on('connect', () => {
-//     console.log(`Sub Connection to ${port} established`)
-//     ZMQsub.subscribe('');
-//   });
-
-//   ZMQsub.on('message', (message) => {
-//     cb(message);
-//   });
-// };
 
 /**
  * subscribeInit
